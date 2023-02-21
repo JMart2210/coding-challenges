b3745557083f343effb0c2fb78ee6b36d473d89b
Jeremy Martin <Jeremy@jeremyandmegan.com>
2023-02-21 14:46:25 -0500

    toFixed number method can specify decimal points for rounding

---
+++ improvingMathRound.js
@@ -0,0 +1,13 @@
+// Improving Math.round(x)
+// Javascript has a handy built-in Math.round(x) method that rounds a number to the nearest integer:
+// Math.round(3.14); // Returns 3
+// Math.round(17.43); // 17
+// Your task is to create a method, Math.roundTo(number, precision) which rounds a given number to precision decimal places.
+// You may find the following Math methods useful:
+// Math.round(x) (of course)
+// Math.pow(x,y) (returns x to the power of y - e.g. Math.pow(3,4) == 81)
+Math.roundTo = function (number, precision) {
+   return Math.round(number * Math.pow(10, precision)) / Math.pow(10, precision)
+}
+
+console.log(Math.roundTo(9.9999, 3));
