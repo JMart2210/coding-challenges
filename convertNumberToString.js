1a762a25280077fde887f95bb96717a1584091d8
Jeremy Martin <Jeremy@jeremyandmegan.com>
2023-02-21 14:29:47 -0500

    return string using template literal

---
+++ convertNumberToString.js
@@ -0,0 +1,9 @@
+// We need a function that can transform a number (integer) into a string.
+function numberToString(num) {
+  return `${num}`
+}
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
