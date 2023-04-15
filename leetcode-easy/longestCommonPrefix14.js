550e69f8ac8f319b2354f947f56915f9a75bded3
Jeremy Martin <46061444+JMart2210@users.noreply.github.com>
2023-04-15 14:23:47 -0400

    nested loop comparisons

---
+++ leetcode-easy/longestCommonPrefix14.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/longest-common-prefix/
+// Write a function to find the longest common prefix string amongst an array of strings.
+// If there is no common prefix, return an empty string "".
+var longestCommonPrefix = function(strs) {
+    //create a loop the length of the first word
+  for (let i= 0; i < strs[0].length; i++){
+    // create loop to length of word list
+    for (let j = 0; j < strs.length - 1; j++){
+      // compare the first letter of word 1 to first letter of each word
+      if (strs[j][i] !== strs[j+1][i]) return strs[0].slice(0,i);
+    }
+  }
+  return strs[0];
+}; 
+
+// Example 1:
+console.log(longestCommonPrefix( ["flower","flow","flight"]), 'fl');
+// Example 2:
+console.log(longestCommonPrefix( ["dog","racecar","car"] ), 'blank') // Output: ""
+console.log(longestCommonPrefix(['flower', 'flow']), 'flow')
