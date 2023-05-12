a08e0bad0c51526795e41f49d8b83f3f34f32b87
Jeremy Martin <46061444+JMart2210@users.noreply.github.com>
2023-05-12 07:52:16 -0400

    set practice

---
+++ exercism/ozansPlaylist.js
@@ -0,0 +1,58 @@
+// https://exercism.org/tracks/javascript/exercises/ozans-playlist
+/**
+ * Removes duplicate tracks from a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} new playlist with unique entries
+ */
+export function removeDuplicates(playlist) {
+  return [...new Set(playlist)];
+  
+}
+/**
+ * Checks whether a playlist includes a track.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {boolean} whether the track is in the playlist
+ */
+export function hasTrack(playlist, track) {
+  return playlist.includes(track)
+}
+/**
+ * Adds a track to a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function addTrack(playlist, track) {
+  let playSet = new Set(playlist);
+  playSet.add(track);
+  return [...playSet];
+}
+/**
+ * Deletes a track from a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function deleteTrack(playlist, track) {
+  let playSet = new Set(playlist);
+  playSet.delete(track);
+  return [...playSet];
+}
+/**
+ * Lists the unique artists in a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} list of artists
+ */
+export function listArtists(playlist) {
+  let artists = new Set();
+  playlist.forEach( song => {
+    artists.add(song.split(' - ')[1])
+  })
+  return [...artists];
+}
